f03ca87a9a105a0e49cbe4c23fccade9ec5a5112
UUNIT <info@uunit.ru>
2024-03-29 09:32:02 +0500

    added pieces

--- main.py
+++ main.py
@@ -1,62 +1,35 @@
-from enum import Enum
-
-
-class Color(Enum):
-    BLACK = 0
-    WHITE = 1
-
-
-class Board:
-    def __init__(self):
-        self.__field = []
-        self.__color = Color.WHITE
-        for _ in range(8):
-            self.__field.append([None] * 8)
-
-    @property
-    def current_player(self):
-        return self.__color
-
-    @property
-    def field(self):
-        return tuple([tuple(row) for row in self.__field])
-
-    def get_piece(self, row: int, col: int):
-        if 1 <= row <= 9 and 1 <= col <= 9:
-            return self.field[row - 1][col - 1]
-        return None
-
-
-
-def convert_step(step: str) -> tuple | Exception:
-    if len(step) != 2:
-        raise Exception('Неверный формат шага (пример A8')
-    s, n = step[0], step[1]
-    if ord(s) < ord('A') or ord(s) > ord('H'):
-        raise Exception('Допустимые символы: A-H')
-    if ord(n) < ord('1') or ord(n) > ord('8'):
-        raise Exception('Допустимые цифры: 1-8')
-    return int(n), ord(s) - 64
+from src import Color, Board
 
 
 def print_board(board: Board):
-    print('     +' + '----+' * 8)
-    for y in range(8, 0, -1):
-        print(f'  {y}  |', end='')
-        for x in range(1, 9):
-            print(' ', end='')
-            piece = board.get_piece(y, x)
+    print('   +' + '----+' * 8)
+    for row in range(8, 0, -1):
+        print(f' {row} ', end='')
+        for col in range(1, 9):
+            print('| ', end='')
+            piece = board.get_piece(row, col)
             if piece is None:
                 print('  ', end='')
             else:
                 print(piece, end='')
-            print(' |', end='')
-        print()
-        print('     +' + '----+' * 8)
-    print(' ' * 5, end='')
-    for x in range(8):
-        print(f'  {chr(65 + x)}  ', end='')
+            print(' ', end='')
+        print('|')
+        print('   +' + '----+' * 8)
+    print('    ', end='')
+    for col in range(8):
+        print(f' {chr(65 + col)}   ', end='')
     print()
+
+
+def convert_step(step: str) -> tuple | Exception:
+    if len(step) != 2:
+        raise Exception('Неверный формат хода')
+    s, n = step[0], step[1]
+    if ord(s) < ord('A') or ord(s) > ord('H'):
+        raise Exception('Неверный формат столбца')
+    if ord(n) < ord('1') or ord(n) > ord('8'):
+        raise Exception('Неверный формат строки')
+    return int(n), ord(s) - ord('A') + 1
 
 
 def main():
@@ -64,13 +37,13 @@ def main():
     while True:
         print_board(board)
         print('Команды:')
-        print('    exit                 --выход')
-        print('    move <from> <to>     --ход из клетки <from> в клетку <to>')
+        print('    exit                --выход')
+        print('    move <from> <to>    --ход из клетки <from> в клетку <to>')
         if board.current_player == Color.WHITE:
-            print('Ходят белые')
+            print('Ход белых')
         else:
-            print('Ходят черные')
-        command = input('Введите команду: ')
+            print('Ход черных')
+        command =input('Введите команду: ')
         if command == 'exit':
             break
         command = command.split()
@@ -78,13 +51,11 @@ def main():
             try:
                 start = convert_step(command[1])
                 end = convert_step(command[2])
-                print(start, end)
-                # ход
-            except Exception as e:
-                print('Ошибка:', e)
+            except Exception as err:
+                print('Ошибка:', err)
             finally:
                 continue
-        print('Неверная команда!')
+        print('Неверная команда. Повторите снова')
 
 
 main()
